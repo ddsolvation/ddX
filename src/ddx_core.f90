60a02e6af9136d362613dcc23e440c4591c08e5c
Michele Nottoli <michele.nottoli@gmail.com>
2025-09-29 13:34:25 +0200

    Bump version: 0.6.1 → 0.6.2

--- src/ddx_core.f90
+++ src/ddx_core.f90
@@ -2789,7 +2789,7 @@ subroutine get_banner(string)
     implicit none
     character (len=2047), intent(out) :: string
     character (len=10) :: vstr
-    write(vstr, *) "0.6.1"
+    write(vstr, *) "0.6.2"
     write(string, *) &
         & " +----------------------------------------------------------------+", &
         & NEW_LINE('a'), &
